f1e577a4b37437818448b9eda21227c1ad18f80a
radias <diasrodri@gmail.com>
2020-03-25 00:52:05 -0300

    contato adicionado

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
 git remote add origin https://github.com/diasrodri/diasrodri.github.io.git
+git add .
+git status
+git commit -m "Arquivos iniciais"
+
+git config --global user.email "diasrodri@gmail.com"
+git config --global user.name "radias"
+
+git push origin master
